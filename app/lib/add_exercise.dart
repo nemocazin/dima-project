bd4808329c1ba4bcd4bade01b655495ea2831cbd
Norronas <nemo.cazin@gmail.com>
2024-12-27 22:38:53 +0100

    [add] : add exercises selection page with filter

--- app/lib/add_exercise.dart
+++ app/lib/add_exercise.dart
@@ -9,7 +9,8 @@ class AddExercisePage extends StatefulWidget {
 
 class _AddExercisePageState extends State<AddExercisePage> {
   List<List<dynamic>> exercises = [];
-  List<List<dynamic>> filteredExercises = [];
+  List<String> exercisesNames = [];
+  List<String> filteredExercisesNames = [];
   TextEditingController searchController = TextEditingController();
 
   @override
@@ -24,7 +25,10 @@ class _AddExercisePageState extends State<AddExercisePage> {
       final csvData = await file.readAsString();
       setState(() {
         exercises = const CsvToListConverter().convert(csvData);
-        filteredExercises = List.from(exercises);
+        for (int i = 1; i < exercises.length; i++) {
+          exercisesNames.add(exercises[i][1].toString());
+        }
+        filteredExercisesNames = List.from(exercisesNames); // Initialiser la liste filtrée
       });
     } catch (e) {
       print('Error loading exercises: $e');
@@ -33,23 +37,27 @@ class _AddExercisePageState extends State<AddExercisePage> {
 
   void filterExercises(String query) {
     setState(() {
-      filteredExercises = exercises.where((exercise) {
-        final name = exercise[0].toString().toLowerCase();
-        return name.contains(query.toLowerCase());
+      filteredExercisesNames = exercisesNames.where((name) {
+        return name.toLowerCase().startsWith(query.toLowerCase());
       }).toList();
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    // Récupérer la hauteur de l'écran
+    double screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: const Color(0xFF1c1e22),
         leading: IconButton(
           icon: Icon(Icons.arrow_back),
+          color: Colors.blue.shade100,
           onPressed: () => Navigator.pop(context),
         ),
-        title: Text('Add Exercise'),
       ),
+      backgroundColor: const Color(0xFF1c1e22),
       body: Column(
         children: [
           Padding(
@@ -58,10 +66,44 @@ class _AddExercisePageState extends State<AddExercisePage> {
               controller: searchController,
               decoration: InputDecoration(
                 labelText: 'Search exercise',
-                prefixIcon: Icon(Icons.search),
+                labelStyle: TextStyle(color: Colors.white), // Change la couleur du label
+                prefixIcon: Icon(Icons.search, color: Colors.white), // Change la couleur de l'icône
                 border: OutlineInputBorder(),
               ),
-              onChanged: filterExercises,
+              style: TextStyle(color: Colors.white), // Change la couleur du texte du champ de recherche
+              onChanged: filterExercises, // Appelle la méthode pour filtrer
+            ),
+          ),
+
+          // Container avec un écart et des coins arrondis
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0), // Ajoute un écart horizontal
+            child: Container(
+              height: screenHeight / 2, // Fixe la hauteur à la moitié de l'écran
+              decoration: BoxDecoration(
+                color: const Color(0xFF242b35), // Couleur de fond pour la liste
+                borderRadius: BorderRadius.circular(20.0), // Coins arrondis
+              ),
+              child: ListView.builder(
+                itemCount: filteredExercisesNames.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(
+                      filteredExercisesNames[index],
+                      style: TextStyle(
+                        color: Colors.white,  // Couleur du texte (blanc)
+                      ),
+                    ),
+                    onTap: () {
+                      Navigator.pushNamed(
+                        context, 
+                        'exerciseSettingsPage',
+                        arguments: filteredExercisesNames[index],
+                      );
+                    },
+                  );
+                },
+              ),
             ),
           ),
 
@@ -70,27 +112,10 @@ class _AddExercisePageState extends State<AddExercisePage> {
             alignment: Alignment.centerLeft,
             child: Text(
               'Filters',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white), // Couleur du texte
             ),
           ),
-
-          Expanded(
-            child: ListView.builder(
-              itemCount: filteredExercises.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(filteredExercises[index][0].toString()),
-                  onTap: () {
-                    Navigator.pushNamed(
-                      context, 
-                      'exerciseSettingsPage',
-                      arguments: filteredExercises[index],
-                    );
-                  },
-                );
-              },
-            ),
-          ),
+          
         ],
       ),
     );
